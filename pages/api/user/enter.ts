6032c38402d9958318d463f7335a58fd5a58b9b8
sunwha.oh <sunwha.oh@adcapsule.co.kr>
2024-04-18 17:54:27 +0900

    Accounts Logic

--- pages/api/user/enter.ts
+++ pages/api/user/enter.ts
@@ -1,8 +1,62 @@
+import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
+  // let user;
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (!user) {
+  //     console.log("Creating user");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   } else {
+  //     console.log("found user by email");
+  //   }
+  //   console.log(user);
+  // }
+
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (!user) {
+  //     console.log("Creating user");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   } else {
+  //     console.log("found user by phone");
+  //   }
+  //   console.log(user);
+  // }
   return res.status(200).end();
 }
 
